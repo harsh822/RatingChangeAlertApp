4c70a149f9cf24d85cf6ec696936aa79797617bf
harsh822 <hp996847@gmail.com>
2022-07-11 17:16:44 +0530

    Added database functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from './user';
 import { ContestDetailsService } from './contest-details.service';
 import { EmailService } from './email.service';
+import { UserDetailsService } from './user-details.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,16 +11,31 @@ import { EmailService } from './email.service';
 export class AppComponent {
   title = 'rating-change-alert-app';
   userModel = new User('', '');
-  mySet = new Set();
+  // mySet = new Set();
+  users: string[] = [];
   onSubmit(form: any) {
-    if (!this.mySet.has(this.userModel)) {
-      this.mySet.add(this.userModel);
-    }
-    this.userModel = new User('', '');
-    console.log(this.mySet);
+    console.log(this.userModel);
+    const data = {
+      handle: this.userModel.handle,
+      email: this.userModel.email,
+    };
+    this.userService.create(data).subscribe(
+      (response) => {
+        // console.log(data);
+        console.log(response);
+        console.log('Andaar');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
     form.reset();
   }
-  constructor(private contestDetail: ContestDetailsService,private emailService:EmailService) {
+  constructor(
+    private contestDetail: ContestDetailsService,
+    private emailService: EmailService,
+    private userService: UserDetailsService
+  ) {
     // var isTrue = false;
     var c = 0;
     setInterval(
@@ -32,11 +48,25 @@ export class AppComponent {
             console.log(entry);
             // console.log(Object.entries(data)[1][1]);
             console.log(Object.entries(response)[0][1]);
+            this.userService.getAll().subscribe(
+              (data) => {
+                this.users = data;
+                console.log(data);
+                console.log(this.users);
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
             for (const key in entry) {
-              if (entry[key].handle === 'harsh822')
-                // var email = ;
-                this.sendEmail(entry[key],"hp996847@gmail.com"); 
-                // console.log(entry[key].oldRating + '  ' + entry[key].newRating);
+              // console.log(this.users[0]);
+              for (let index = 0; index < this.users.length; index++) {
+                const element = Object.values(this.users[index]);
+                // console.log(element[1]);
+                if(entry[key].handle === element[1]) {
+                  this.sendEmail(entry[key], element[2]);
+                }
+              }
             }
           },
           (error) => {
@@ -47,17 +77,16 @@ export class AppComponent {
       10000
     );
   }
-  sendEmail(ratings:any,email:any) {
-    console.log("Hetere");
-    console.log(ratings.oldRating+" "+ratings.newRating+" "+email);
+  sendEmail(ratings: any, email: any) {
+    console.log('Here');
+    console.log(ratings.oldRating + ' ' + ratings.newRating + ' ' + email);
     let reqObj = {
-      email:email,
-      oldRating:ratings.oldRating,
-      newRating:ratings.newRating
-    }
-    this.emailService.sendMessage(reqObj).subscribe(data=>{
+      email: email,
+      oldRating: ratings.oldRating,
+      newRating: ratings.newRating,
+    };
+    this.emailService.sendMessage(reqObj).subscribe((data) => {
       console.log(data);
-    })
+    });
   }
-  
 }
